e3f52562083e866116152c1e5c088ce5bbed535f
Alex Blong <alexanderblong07@gmail.com>
2025-03-31 11:18:40 +1300

    Changed gradient setup

--- Social Widgets/BackGroundView.swift
+++ Social Widgets/BackGroundView.swift
@@ -2,50 +2,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-            List {
-                VStack {
-                    
-                    Spacer()
-                    HStack {
-                    
-                        Image("Group 13") // Left
-                            .resizable() // Make the image resizable
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                            .background(Color(red: 0.50, green: 0.50, blue: 0.50))
-                            .cornerRadius(15)
-
-                        Spacer() // Between left and right
+        
+        GeometryReader { geometry in
+            RoundedRectangle(cornerRadius: 25)
+                .fill(
+                    LinearGradient(gradient:
+                                Gradient(colors : [
+                                    Color.green.opacity(0.8),
+                                    Color.blue.opacity(0.8),
+                                    
+                                    
+                                ]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                    )
+                                   
+                                    
+        
+           
+            
+            
+            
+            NavigationStack {
+                List {
+                    VStack {
+                        
+                        Spacer()
+                        HStack {
                             
-                        Image(systemName: "plus.square") // Center
-                            .resizable() // Make the image resizable
-                            .scaledToFit()
-                            .frame(width: 46, height: 46)
-                           //  .cornerRadius(15)
-                                
-                        Spacer() // Center and right
-                                
-                        Image(systemName: "gear") // Right
-                            .resizable() // Make the image resizable
-                            .scaledToFit() // Scale the image to fit its frame
-                            .frame(width: 50, height: 50)
-                            .cornerRadius(15)
-
+                            Image("Group 13") // Left
+                                .resizable() // Make the image resizable
+                                .scaledToFit()
+                                .frame(width: 60, height: 50)
+                                .background(Color(red: 0.50, green: 0.50, blue: 0.50))
+                                .cornerRadius(15)
+                            
+                            Spacer() // Between left and right
+                            
+                            Image(systemName: "plus.square") // Center
+                                .resizable() // Make the image resizable
+                                .scaledToFit()
+                                .frame(width: 46, height: 46)
+                            //  .cornerRadius(15)
+                            
+                            Spacer() // Center and right
+                            
+                            Image(systemName: "gear") // Right
+                                .resizable() // Make the image resizable
+                                .scaledToFit() // Scale the image to fit its frame
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(15)
+                            
+                            
+                        }
+                      //  .padding(0) // Add padding around the HStack
+                      //  .padding(.top, 650) // Add additional padding to the top
+                        
                         
                     }
-                    .padding(0) // Add padding around the HStack
-                    .padding(.top, 650) // Add additional padding to the top
-
+                    .navigationTitle("Social Widgets")
+                    Button(action: {
+                        // Define the action here, like print("Button tapped")
+                    }) {
+                        Text("Click Me") // Content of the button
+                    }
                     
                 }
-                .navigationTitle("Social Widgets")
-                Button(action: {
-                    // Define the action here, like print("Button tapped")
-                }) {
-                    Text("Click Me") // Content of the button
-                }
-                
             }
         }
     }
--- Social Widgets/Social_WidgetsApp.swift
+++ Social Widgets/Social_WidgetsApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct Social_WidgetsApp: App {
     var body: some Scene {
         WindowGroup {
+            ContentView()
         }
     }
 }
